7b81e71872212a764f9e74d5f0047fbf52118c9f
Caio Nascimento <caionsantos2000@gmail.com>
2024-04-24 00:25:41 -0300

    notification ok

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,25 @@
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyBdWss7LxBLZ9P9mzhW8Ha8FdL1q2F9Yg8',
+  authDomain: 'poc-flutter-notification.firebaseapp.com',
+  projectId: 'poc-flutter-notification',
+  storageBucket: 'poc-flutter-notification.appspot.com',
+  messagingSenderId: '980765464706',
+  appId: '1:980765464706:web:f18427882e5a1b38e07c9b',
+});
+
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((message) => {
+  console.log('chegou uma background message aaa')
+  console.log(message.notification.title)
+  console.log(message.notification.body)
+  // Exibir uma notificação usando o método showNotification()
+  // self.registration.showNotification(message.notification.title, {
+  //   body: message.notification.body,
+  //   // Você pode adicionar outras propriedades de configuração da notificação aqui
+  // });
+});
