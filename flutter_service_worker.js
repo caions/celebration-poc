554a4c5565506708acaf0e17ec42309a966bee6f
Caio Nascimento <caionsantos2000@gmail.com>
2024-05-31 13:09:24 -0300

    feat: finished app functionalities

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+  "assets/AssetManifest.json": "6b0bd29a34c97b2fd2ae92e9f8cb6fd8",
+"assets/assets/images/people_cheers.jpeg": "205d6e727f15d4b64decac9b81623540",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
-"assets/NOTICES": "2500e7e0f137bf7df9c5470b927cc773",
+"assets/NOTICES": "a3cba4c17219066e2000a65015a0dee2",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/shaders/ink_sparkle.frag": "ae6c1fd6f6ee6ee952cde379095a8f3f",
 "canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
@@ -20,9 +21,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "4710137331aa5a13ae0f6610f3bbead0",
-"/": "4710137331aa5a13ae0f6610f3bbead0",
-"main.dart.js": "d8289ebdcd67b59f401f532dced7acea",
+"index.html": "481c0edfe60c0b91a92f4637c9d994e1",
+"/": "481c0edfe60c0b91a92f4637c9d994e1",
+"main.dart.js": "5966ebb378020bf58dd1af580b457e81",
 "manifest.json": "b6382d9d9c84e72609c7cfd61e7b2847",
 "version.json": "75f796d57563ea707bec37e4cadbd86b"
 };
