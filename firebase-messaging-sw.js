0388336ef55df38843b8c06cd73146c2db2b110a
Caio Nascimento <caionsantos2000@gmail.com>
2024-06-02 22:51:52 -0300

    feat: version-1.1.2 - duplicated notification push v5

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,18 +2,47 @@ importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
 
 firebase.initializeApp({
-  //credenciais
+  apiKey: 'AIzaSyBdWss7LxBLZ9P9mzhW8Ha8FdL1q2F9Yg8',
+  authDomain: 'poc-flutter-notification.firebaseapp.com',
+  projectId: 'poc-flutter-notification',
+  storageBucket: 'poc-flutter-notification.appspot.com',
+  messagingSenderId: '980765464706',
+  appId: '1:980765464706:web:f18427882e5a1b38e07c9b',
 });
 
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((message) => {
-  console.log('chegou uma background mensagem')
-  console.log(message.notification.title)
-  console.log(message.notification.body)
-});
+// Manipulador de evento para notificações
+self.addEventListener('notificationclick', function (event) {
+  console.log('[firebase-messaging-sw.js] Notification click Received.');
 
-messaging.addEventListener('notificationclick', function (event) {
   event.notification.close();
-  clients.openWindow("/");
+
+  // Verifique se a notificação possui um URL (se sim, redireciona para lá)
+  if (event.notification.data && event.notification.data.url) {
+    const urlToOpen = new URL(event.notification.data.url, self.location.origin).href;
+
+    const promiseChain = clients.matchAll({
+      type: 'window',
+      includeUncontrolled: true
+    }).then((windowClients) => {
+      let matchingClient = null;
+
+      for (let i = 0; i < windowClients.length; i++) {
+        const windowClient = windowClients[i];
+        if (windowClient.url === urlToOpen) {
+          matchingClient = windowClient;
+          break;
+        }
+      }
+
+      if (matchingClient) {
+        return matchingClient.focus();
+      } else {
+        return clients.openWindow(urlToOpen);
+      }
+    });
+
+    event.waitUntil(promiseChain);
+  }
 });
