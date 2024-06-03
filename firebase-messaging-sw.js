0c4f49ec9773c4e65d1d4e365dce070cf792ef40
Caio Nascimento <caionsantos2000@gmail.com>
2024-06-02 22:57:13 -0300

    feat: version-1.1.2 - duplicated notification push v6

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -11,38 +11,7 @@ firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
-
-// Manipulador de evento para notificações
-self.addEventListener('notificationclick', function (event) {
-  console.log('[firebase-messaging-sw.js] Notification click Received.');
-
+messaging.addEventListener('notificationclick', function (event) {
   event.notification.close();
-
-  // Verifique se a notificação possui um URL (se sim, redireciona para lá)
-  if (event.notification.data && event.notification.data.url) {
-    const urlToOpen = new URL(event.notification.data.url, self.location.origin).href;
-
-    const promiseChain = clients.matchAll({
-      type: 'window',
-      includeUncontrolled: true
-    }).then((windowClients) => {
-      let matchingClient = null;
-
-      for (let i = 0; i < windowClients.length; i++) {
-        const windowClient = windowClients[i];
-        if (windowClient.url === urlToOpen) {
-          matchingClient = windowClient;
-          break;
-        }
-      }
-
-      if (matchingClient) {
-        return matchingClient.focus();
-      } else {
-        return clients.openWindow(urlToOpen);
-      }
-    });
-
-    event.waitUntil(promiseChain);
-  }
+  clients.openWindow("/");
 });
